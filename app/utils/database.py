9ed0edae207b06cc1ef05619a17fa483cfb1ea56
Kingsley Ugwudinso <kingholyhill@gmail.com>
2023-11-21 20:38:02 +0100

    initialised project

--- app/utils/database.py
+++ app/utils/database.py
@@ -1,6 +1,7 @@
 import motor.motor_asyncio
+from .config import config
 
 def create_db():
-        client = motor.motor_asyncio.AsyncIOMotorClient("MONGODB_URL")
+        client = motor.motor_asyncio.AsyncIOMotorClient(config.database.dsn)
         db = client.wellgab
         return db
