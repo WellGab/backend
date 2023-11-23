eca09d8474dcab9e193ce944646b69f5240c5e71
Kingsley Ugwudinso <kingholyhill@gmail.com>
2023-11-23 23:13:56 +0100

    merged signup

--- app/utils/mongodb.py
+++ app/utils/mongodb.py
@@ -5,9 +5,7 @@ class Mongodb():
     def __init__(self, dsn: str, db: str):
         self.db_name = db
         self.conn = connect(host=dsn)
-        # print("///////////////////////////////////////////////// conn ", self.conn)
         self.db = self.conn[db]
-        # print("///////////////////////////////////////////////// db ", self.db)
 
     def get_db(self):
         return self.db
