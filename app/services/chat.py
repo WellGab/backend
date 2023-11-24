c1115b59c2006ea4afca71bdc4211837d2eb71d2
Kingsley Ugwudinso <kingholyhill@gmail.com>
2023-11-24 23:31:22 +0100

    bug fixes

--- app/services/chat.py
+++ app/services/chat.py
@@ -11,21 +11,21 @@ class ChatService:
 
         prompt = config.BASE_PROMPT + message
 
-        stream = await client.chat.completions.create(
-            messages=[{"role": "assistant", "content": prompt,}],
-            model="gpt-3.5-turbo",
-            temperature= 0.5,
-            max_tokens=1024, 
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0,
-            stream=True
-        )
+        try:
+            stream = await client.chat.completions.create(
+                messages=[{"role": "assistant", "content": prompt,}],
+                model="gpt-3.5-turbo",
+                temperature= 0.5,
+                max_tokens=1024, 
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0,
+            )
+            response = stream.choices[0].message.content
+            return response.strip()
+        except Exception:
+            return "Pardon!"
 
-        response = ""
+        
 
-        async for part in stream:
-            content = part.choices[0].delta.content or ""
-            response = f'{response}{content}'
-
-        return response.strip()
+        
