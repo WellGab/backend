b0c2972c9f29823a5ee54639382b8f9d038dd26b
Kingsley Ugwudinso <kingholyhill@gmail.com>
2023-11-24 19:23:35 +0100

    implemented socketio

--- app/services/chat.py
+++ app/services/chat.py
@@ -4,9 +4,9 @@ from openai import OpenAI
 class ChatService:
     @staticmethod
     def send_message(message: str) -> str:
-        client = OpenAI(
-            api_key='org-Zf5QvdNsItZESXSssUF77VRZ',
-        )
+        # client = OpenAI(
+        #     api_key='org-Zf5QvdNsItZESXSssUF77VRZ',
+        # )
 
         # chat_completions = client.chat.completions.create()
         # print("Reply: ", chat_completions)
