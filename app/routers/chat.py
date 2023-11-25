f30764bd1a1e5e0ae26acb37144e3c0a637bdb63
Nwokoye Chigozie <gregoflash05@gmail.com>
2023-11-25 10:25:35 +0100

    added create chat endpoint

--- app/routers/chat.py
+++ app/routers/chat.py
@@ -11,3 +11,8 @@ router = APIRouter(prefix=config.CHAT_URL, tags=["Chats"])
 @router.get("/conversations", response_model=chat_schema.ConversationResponse)
 def get_user_conversations(user_id: str = Depends(AuthService.get_current_user_id), page_number: int = 1, page_size: int = 50):
     return chat_controller.ChatController.get_user_conversations(user_id, page_number, page_size)
+
+
+@router.post("/chat", response_model=chat_schema.CreateChatResponse)
+def create_chat(req_data: chat_schema.CreateChatSchema, user_id: str = Depends(AuthService.get_current_user_id)):
+    return chat_controller.ChatController.create_chat(user_id, req_data)
